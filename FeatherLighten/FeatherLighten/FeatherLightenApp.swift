631b3ae80d66da11bc688e1e838dd4a0b34271fa
Michele Feola <michele.feola@studenti.unisob.na.it>
2026-02-25 11:32:57 +0100

    first commit

---
+++ FeatherLighten/FeatherLighten/FeatherLightenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeatherLightenApp.swift
+//  FeatherLighten
+//
+//  Created by Foundation 35 on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FeatherLightenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
