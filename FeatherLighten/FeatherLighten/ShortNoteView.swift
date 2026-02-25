a30191c9b9207744172938e8ae93ec2f64c2b372
Flavia <flaviettads@gmail.com>
2026-02-25 11:42:52 +0100

    second commit

---
+++ FeatherLighten/FeatherLighten/ShortNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  ShortNoteView.swift
+//  FeatherLighten
+//
+//  Created by Foundation 23 on 25/02/26.
+//
+
+import SwiftUI
+
+struct ShortNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShortNoteView()
+}
